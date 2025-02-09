e4f844a02beb783e0fd8f1e1271443a5265d05e4
abdelrhman-saeed <abdelrhmansaeed001@gmail.com>
2025-02-09 21:56:57 +0200

    refactor: improved the FlightsController class

--- app/Http/Controllers/FlightsController.php
+++ app/Http/Controllers/FlightsController.php
@@ -9,102 +9,78 @@ use App\Models\TokenID;
 use Laravel\Sanctum\PersonalAccessToken;
 use Log;
 
-
 class FlightsController extends Controller
 {
     private PersonalAccessToken $token;
     private TokenID $tokenID;
-    private float $additionalPricingPercentage = 0.5 / 100;
+    private float $pricingPercentage = 0.5 / 100;
 
     public function __construct()
     {
-        $this->token      = auth()->user()->currentAccessToken();
-        $this->tokenID    = TokenID::where('token_id', $this->token['id'])->firstOrFail();
+        $this->token    = auth()->user()->currentAccessToken();
+        $this->tokenID  = TokenID::where('token_id', $this->token['id'])->firstOrFail();
     }
 
-    private function externalAPICall(string $endoint, array $data)
+    private function externalAPICall(string $endpoint, array $data): array
     {
-        $response = Http::withToken($this->tokenID->access_token)
-                        ->withHeader('ama-client-ref', $this->tokenID->uuid)
-                        ->post($endoint, $data)
-                        ->json();
+        Log::channel('request')->info("User API Call", [
+            'user_id'        => auth()->user()->id,
+            'ama_client_ref' => $this->tokenID->uuid,
+            'endpoint'       => $endpoint
+        ]);
 
-        Log::channel('request')->info("User Api Call", [
-                'User'              => auth()->user()->id,
-                'ama_client_ref'    => $this->tokenID->uuid,
-                'endpoint'          => $endoint
-            ]);
+        return Http::withToken($this->tokenID->access_token)
+                ->withHeader('ama-client-ref', $this->tokenID->uuid)
+                ->post($endpoint, $data)
+                ->json();
+    }
 
-        return $response;
+    private function adjustPricing(array &$price, bool $increase = true): void
+    {
+        $factor = $increase ? (1 + $this->pricingPercentage) : (1 - $this->pricingPercentage);
+
+        $price['total'] = round($price['total'] * $factor, 2);
+        $price['base']  = round($price['base']  * $factor, 2);
+
+        if (isset($price['grandTotal'])) {
+            $price['grandTotal'] = round($price['grandTotal'] * $factor, 2);
+        }
     }
 
     public function offers(FlightOfferRequest $request): array
     {
-        $result = $this->externalAPICall(
-                    env('AMADEUS_FLIGHT_OFFERS'),
-                    $request->validated()
-                );
+        $result = $this->externalAPICall(env('AMADEUS_FLIGHT_OFFERS'), $request->validated());
 
-        foreach($result['data'] as &$offer)
-        {
-            $offer['price'] = $this->addAdditionalPricingPercentage($offer['price']);
+        foreach ($result['data'] as &$offer) {
+            $this->adjustPricing($offer['price']);
 
-            foreach($offer['travelerPricings'] as &$travelerPricing) {
-                $travelerPricing['price'] = $this->addAdditionalPricingPercentage($travelerPricing['price']);
+            foreach ($offer['travelerPricings'] as &$travelerPricing) {
+                $this->adjustPricing($travelerPricing['price']);
             }
         }
 
         return $result;
     }
 
-    private function addAdditionalPricingPercentage(array $price): array
-    {
-        $price['total'] += $price['total'] * $this->additionalPricingPercentage;
-        $price['base']  += $price['base']  * $this->additionalPricingPercentage;
-
-        if (isset($price['grandTotal'])) {
-            $price['grandTotal'] += $price['grandTotal'] * $this->additionalPricingPercentage;
-        }
-
-        return $price;
-    }
-
-    private function restorePricing(array $price): array
-    {
-        $price['total'] -= $price['total'] * $this->additionalPricingPercentage;
-        $price['base']  -= $price['base']  * $this->additionalPricingPercentage;
-
-        if (isset($price['grandTotal'])) {
-            $price['grandTotal'] -= $price['grandTotal'] * $this->additionalPricingPercentage;
-        }
-
-        return $price;
-    }
-
     public function pricing(FightPricingRequest $request): array
     {
         $validated = $request->validated();
 
-        foreach($validated['data']['flightOffers'] as &$offer)
-        {
-            $offer['price'] = $this->restorePricing($offer['price']);
+        foreach ($validated['data']['flightOffers'] as &$offer) {
+            $this->adjustPricing($offer['price'], false);
 
-            foreach($offer['travelerPricings'] as &$travelerPricing) {
-                $travelerPricing['price'] = $this->restorePricing($travelerPricing['price']);
+            foreach ($offer['travelerPricings'] as &$travelerPricing) {
+                $this->adjustPricing($travelerPricing['price'], false);
             }
         }
 
-        $result = $this->externalAPICall(
-                    env('AMADEUS_FLIGHT_PRICING'),
-                    $validated
-                );
+        $result = $this->externalAPICall(env('AMADEUS_FLIGHT_PRICING'), $validated);
 
-        foreach($result['data']['flightOffers'] as &$offer)
-        {
-            $offer['price'] = $this->addAdditionalPricingPercentage($offer['price']);
-
-            foreach($offer['travelerPricings'] as &$travelerPricing) {
-                $travelerPricing['price'] = $this->addAdditionalPricingPercentage($travelerPricing['price']);
+        foreach ($result['data']['flightOffers'] as &$offer) {
+            $this->adjustPricing($offer['price']);
+            
+            foreach ($offer['travelerPricings'] as &$travelerPricing) {
+                $this->adjustPricing($travelerPricing['price']);
             }
         }
 
